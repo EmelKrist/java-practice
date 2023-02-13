3571db0202a516c8c9ef4a945ee7ea39d685d084
EmelKrist <kristina_1516@mail.ru>
2023-02-13 16:05:46 +0800

    feature(Eight_kyu): reversedWords method and its alternatives

--- CodewarsPractice/src/Eight_kyu.java
+++ CodewarsPractice/src/Eight_kyu.java
@@ -1,5 +1,66 @@
+import java.util.Scanner;
+
 public class Eight_kyu {
     public static void main(String[] args) {
+        //reverseWordsVersionThree();
+    }
 
+    public static void reversedWords() {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine(); // вводим строку слов
+
+        String[] arrWords = inputString.split(" "); // сплитуем в массив по пробелам
+        char[] charWord;
+        for (int i = 0; i < arrWords.length; i++) {
+            charWord = arrWords[i].toCharArray(); // преобразуем каждое слово в массив в массив символов
+            for (int j = 0; j < charWord.length / 2; j++) {
+                // меняем первый элемент в масиве символов с последним
+                char temp = charWord[j];
+                charWord[j] = charWord[charWord.length - j - 1];
+                charWord[charWord.length - j - 1] = temp;
+            }
+            String resultWord = "";
+            for (int k = 0; k < charWord.length; k++) {
+                // соединяем массив символов в развернутое слово
+                resultWord += charWord[k];
+            }
+            arrWords[i] = resultWord; // заносим развернутое слово в массив слов и выводим
+            System.out.print(arrWords[i] + " ");
+        }
+    }
+
+    public static void reverseWordsVersionTwo() {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+
+        String[] arrWords = inputString.split(" ");
+        String lastWord, reverseWord;
+        for (int i = 0; i < arrWords.length; i++) {
+            lastWord = arrWords[i];
+            reverseWord = "";
+            for (int ch = lastWord.length() - 1; ch >= 0; ch--) {
+                reverseWord += lastWord.charAt(ch);
+            }
+            System.out.print(reverseWord + " ");
+        }
+    }
+
+    /*
+    Complete the solution so that it reverses all of the words within the string passed in.
+    Words are separated by exactly one space and there are no leading or trailing spaces.
+
+    Заметка: эта альтренатива - правильное решение задачи, так как
+    последние слова должны становиться первыми(изначально неправильно поняла задачу)
+     */
+    public static void reverseWordsVersionThree() {
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+
+        String[] arrWords = inputString.split(" ");
+        String reverseString = "";
+        for (int i = arrWords.length - 1; i >= 0; i--) {
+            reverseString += arrWords[i] + " ";
+        }
+        System.out.println(reverseString.substring(0, reverseString.length() - 1));
     }
 }
