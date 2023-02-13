798f9048f7228f342c8a66affde4742dd7b45789
EmelKrist <kristina_1516@mail.ru>
2023-02-13 16:30:54 +0800

    feature(Six_kyu): digitalPow method

--- CodewarsPractice/src/Six_kyu.java
+++ CodewarsPractice/src/Six_kyu.java
@@ -3,6 +3,7 @@ public class Six_kyu {
         //System.out.println(camelCase("camelCasing"));
         //System.out.println(bouncingBall(3.0, 0.66, 1.5));
         //System.out.println(isValidWalk(new char[] {'n','s','n','s','n','s','n','s','n','s'});
+        //System.out.println(digitalPow(89,1));
     }
 
     /*
@@ -78,5 +79,27 @@ public class Six_kyu {
             return xStep == 0 && yStep == 0;
         }
         return false;
+    }
+
+    /*
+    https://www.codewars.com/kata/5552101f47fc5178b1000050
+     */
+    public static long digitalPow(int n, int p) {
+        String[] nums = ("" + n).split("");
+        long k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            k += Math.pow(Double.parseDouble(nums[i]), p);
+            p++;
+        }
+        return k % n == 0 ? k / n : -1;
+    }
+
+    public static long digitalPowAlter(int n, int p) {
+        String nStr = String.valueOf(n);
+        long k = 0;
+        for (int i = 0; i < nStr.length(); i++) {
+            k += Math.pow(Character.getNumericValue(nStr.charAt(i)), p++);
+        }
+        return k % n == 0 ? k / n : -1;
     }
 }
