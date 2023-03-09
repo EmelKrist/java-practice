d7887db5ee4b8cc06e95aba90ebef34b2d2cf929
EmelKrist <kristina_1516@mail.ru>
2023-03-09 14:05:00 +0800

    feature(_7kyu): HeronFormula solution

---
+++ CodewarsPractice/src/main/java/_7kyu/HeronFormula.java
@@ -0,0 +1,11 @@
+package _7kyu;
+
+/*
+https://www.codewars.com/kata/57aa218e72292d98d500240f
+ */
+public class HeronFormula {
+    public static double heron(double a, double b, double c) {
+        double s = (a + b + c) / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
