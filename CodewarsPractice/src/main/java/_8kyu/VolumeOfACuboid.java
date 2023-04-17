f6f9f3cfa6ed27a98d1ed82f05cc24466a4ae270
EmelKrist <kristina_1516@mail.ru>
2023-04-17 10:03:31 +0800

    feature(_8kyu): VolumeOfACuboid solution

---
+++ CodewarsPractice/src/main/java/_8kyu/VolumeOfACuboid.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/58261acb22be6e2ed800003a
+ */
+public class VolumeOfACuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+}
