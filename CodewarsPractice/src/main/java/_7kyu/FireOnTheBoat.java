4cebd8ba411b95990c378b554fa22a6e8a311f98
EmelKrist <kristina_1516@mail.ru>
2023-05-06 08:48:50 +0800

    feature(_7kyu): FireOnTheBoat solution

---
+++ CodewarsPractice/src/main/java/_7kyu/FireOnTheBoat.java
@@ -0,0 +1,10 @@
+package _7kyu;
+
+/**
+ * https://www.codewars.com/kata/57e8fba2f11c647abc000944
+ */
+public class FireOnTheBoat {
+    public static String fireFight(String s) {
+        return s.replace("Fire", "~~");
+    }
+}
