828f9c05f327fd03ea47a36ad85342d47ec156f9
EmelKrist <kristina_1516@mail.ru>
2023-05-11 12:19:53 +0800

    feature(easy): Find the Index of the First Occurrence in a String problem

---
+++ LeetcodePractice/src/main/java/easy/FirstOccurrence.java
@@ -0,0 +1,30 @@
+package easy;
+
+/**
+ * https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+ */
+public class FirstOccurrence {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public int strStrAlter(String haystack, String needle) {
+        int hLen = haystack.length();
+        int nLen = needle.length();
+        int nIndex = 0;
+
+        for (int i = 0; i < hLen; i++) {
+            if (haystack.charAt(i) == needle.charAt(nIndex)) {
+                nIndex++;
+            } else {
+                i = i - nIndex;
+                nIndex = 0;
+            }
+
+            if (nIndex == nLen) {
+                return i - nIndex + 1;
+            }
+        }
+        return -1;
+    }
+}
