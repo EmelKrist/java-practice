e0660be6a532bec9f60c50dfc770a451e02b0f4c
EmelKrist <kristina_1516@mail.ru>
2023-05-12 11:30:49 +0800

    feature(_7kyu): SumOfAngles solution

---
+++ CodewarsPractice/src/main/java/_7kyu/SumOfAngles.java
@@ -0,0 +1,10 @@
+package _7kyu;
+
+/**
+ * https://www.codewars.com/kata/5a03b3f6a1c9040084001765
+ */
+public class SumOfAngles {
+    public static int sumOfAngles(int n) {
+        return (n - 2) * 180;
+    }
+}
