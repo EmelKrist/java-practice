8697024fccdc03b457e5fc890667efbe8640152e
EmelKrist <kristina_1516@mail.ru>
2023-05-17 07:46:21 +0800

    feature(_8kyu): FindThePosition solution

---
+++ CodewarsPractice/src/main/java/_8kyu/FindThePosition.java
@@ -0,0 +1,11 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/5808e2006b65bff35500008f
+ */
+public class FindThePosition {
+    public static String position(char alphabet) {
+        System.out.println(alphabet - 60);
+        return "Position of alphabet: " + (alphabet - 60);
+    }
+}
