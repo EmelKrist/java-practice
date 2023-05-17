34eac27e94525a61a97d4542bf1a46b1f5c60df6
EmelKrist <kristina_1516@mail.ru>
2023-05-17 08:07:22 +0800

    feature(_8kyu): RemoveStringSpaces solution

---
+++ CodewarsPractice/src/main/java/_8kyu/RemoveStringSpaces.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+ */
+public class RemoveStringSpaces {
+    public static String noSpace(final String x) {
+        return x.replaceAll("\\s", "");
+    }
+}
