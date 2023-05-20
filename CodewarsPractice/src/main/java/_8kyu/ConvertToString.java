a9b462783b7a8da61eeef5a489c3f48c9f9fd52f
EmelKrist <kristina_1516@mail.ru>
2023-05-20 16:36:15 +0800

    feature(_8kyu): ConvertToString solution and its alternatives

---
+++ CodewarsPractice/src/main/java/_8kyu/ConvertToString.java
@@ -0,0 +1,18 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+ */
+public class ConvertToString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+
+    public static String numberToStringAlter(int num) {
+        return Integer.toString(num);
+    }
+
+    public static String numberToStringAlterTwo(int num) {
+        return ""+num;
+    }
+}
