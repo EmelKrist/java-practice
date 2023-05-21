fb80836d63c4b95b374b894c88113e1e167cc81f
EmelKrist <kristina_1516@mail.ru>
2023-05-21 16:23:20 +0800

    feature(_8kyu): BooleanToString solution

---
+++ CodewarsPractice/src/main/java/_8kyu/BooleanToString.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/551b4501ac0447318f0009cd
+ */
+public class BooleanToString {
+    public static String convert(boolean b) {
+        return String.valueOf(b);
+    }
+}
