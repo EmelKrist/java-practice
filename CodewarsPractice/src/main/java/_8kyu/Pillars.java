7d21f84bb84c5b3726b1c095bed665c35d99fff2
EmelKrist <kristina_1516@mail.ru>
2023-05-28 11:20:54 +0800

    feature(_8kyu): Pillars solution

---
+++ CodewarsPractice/src/main/java/_8kyu/Pillars.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/5bb0c58f484fcd170700063d
+ */
+public class Pillars {
+    public static int pillars(int numPill, int dist, int width){
+        return numPill > 1 ? (numPill - 1) * (dist * 100) + (numPill - 2) * width : 0;
+    }
+}
