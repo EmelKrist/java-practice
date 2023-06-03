cc536592fc3ac5501e41db08a1f79e01b33b84c7
EmelKrist <kristina_1516@mail.ru>
2023-06-03 18:55:13 +0800

    feature(_8kyu): IsItEven solution

---
+++ CodewarsPractice/src/main/java/_8kyu/IsItEven.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/java
+ */
+public class IsItEven {
+    public boolean isEven(double n) {
+        return n % 2 == 0.0;
+    }
+}
