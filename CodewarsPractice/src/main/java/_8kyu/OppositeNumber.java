7643533531e85e56d285aed76ffb7e6768068033
EmelKrist <kristina_1516@mail.ru>
2023-06-05 12:30:48 +0800

    feature(_8kyu): OppositeNumber solution

---
+++ CodewarsPractice/src/main/java/_8kyu/OppositeNumber.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/56dec885c54a926dcd001095
+ */
+public class OppositeNumber {
+    public static int opposite(int number) {
+        return -number;
+    }
+}
