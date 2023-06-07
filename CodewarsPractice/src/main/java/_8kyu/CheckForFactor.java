ce0c66166924e8cf8b4f8378ebf55e13649484fe
EmelKrist <kristina_1516@mail.ru>
2023-06-07 17:48:05 +0800

    feature(_8kyu): MakeUpperCase solution

---
+++ CodewarsPractice/src/main/java/_8kyu/CheckForFactor.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/java
+ */
+public class CheckForFactor {
+    public static boolean checkForFactor(int base, int factor) {
+        return base % factor == 0;
+    }
+}
