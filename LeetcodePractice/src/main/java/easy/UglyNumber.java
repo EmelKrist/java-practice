3c57486260908c66d1624d4966f589b2e1d29a10
EmelKrist <kristina_1516@mail.ru>
2023-06-22 12:14:03 +0800

    feature(easy): Ugly Number problem

---
+++ LeetcodePractice/src/main/java/easy/UglyNumber.java
@@ -0,0 +1,20 @@
+package easy;
+
+/**
+ * https://leetcode.com/problems/ugly-number/
+ */
+public class UglyNumber {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+        while (n > 0 && n % 5 == 0) {
+            n /= 5;
+        }
+        while (n > 0 && n % 3 == 0) {
+            n /= 3;
+        }
+        while (n > 0 && n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
