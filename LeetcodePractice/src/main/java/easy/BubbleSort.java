a94cc73ed6f547091d3733e4697c60743cb9926e
EmelKrist <kristina_1516@mail.ru>
2023-07-01 13:55:12 +0800

    feature(easy): Bubble Sort problem

---
+++ LeetcodePractice/src/main/java/easy/BubbleSort.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class BubbleSort {
+    public static int[] sort(int[] array) {
+        for (int i = array.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
