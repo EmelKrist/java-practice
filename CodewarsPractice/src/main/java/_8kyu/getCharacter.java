ec0f3619689086c2b4ad8af27239d1137b552f92
EmelKrist <kristina_1516@mail.ru>
2023-07-06 21:36:39 +0800

    feature(_8kyu): getCharacter solution

---
+++ CodewarsPractice/src/main/java/_8kyu/getCharacter.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/55ad04714f0b468e8200001c
+ */
+public class getCharacter {
+    public static char getChar(int c) {
+        return (char) c;
+    }
+}
