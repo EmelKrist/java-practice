5a4d7ebda12172cea6a3bd1535ed2b0111328b8c
EmelKrist <kristina_1516@mail.ru>
2023-07-06 21:31:04 +0800

    feature(_8kyu): IsHeGonnaSurvive solution

---
+++ CodewarsPractice/src/main/java/_8kyu/IsHeGonnaSurvive.java
@@ -0,0 +1,10 @@
+package _8kyu;
+
+/**
+ * https://www.codewars.com/kata/59ca8246d751df55cc00014c
+ */
+public class IsHeGonnaSurvive {
+    public static boolean hero(int bullets, int dragons) {
+        return bullets / 2 >= dragons;
+    }
+}
