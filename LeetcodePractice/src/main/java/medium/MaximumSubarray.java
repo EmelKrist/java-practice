5e70a00c5a43a95655238d4791843fd917d4259b
EmelKrist <kristina_1516@mail.ru>
2023-08-24 20:37:45 +0800

    feature(medium): Maximum subarray problem

---
+++ LeetcodePractice/src/main/java/medium/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package medium;
+
+/**
+ * https://leetcode.com/problems/maximum-subarray/
+ */
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+
+            if (currentSum > maxSum)
+                maxSum = currentSum;
+
+            if (currentSum < 0)
+                currentSum = 0;
+        }
+        return maxSum;
+    }
+}
