2857477242b18781124a78f3c9ead839baa30115
EmelKrist <kristina_1516@mail.ru>
2023-10-12 13:51:38 +0800

    feature(medium): Edit Distance problem

---
+++ LeetcodePractice/src/main/java/medium/EditDistance.java
@@ -0,0 +1,29 @@
+package medium;
+
+/**
+ * https://leetcode.com/problems/edit-distance/
+ */
+public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        if (n == 0) return m;
+        if (m == 0) return n;
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
