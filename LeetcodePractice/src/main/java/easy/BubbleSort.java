84274b224fdd09cee1cf472c96db4b514bead651
EmelKrist <kristina_1516@mail.ru>
2023-10-14 22:02:11 +0800

    feature(easy): Bubble Sort alter solution

--- LeetcodePractice/src/main/java/easy/BubbleSort.java
+++ LeetcodePractice/src/main/java/easy/BubbleSort.java
@@ -1,5 +1,6 @@
 package easy;
 
+
 public class BubbleSort {
     public static int[] sort(int[] array) {
         for (int i = array.length - 1; i > 0; i--) {
@@ -8,6 +9,20 @@ public class BubbleSort {
                     int temp = array[j + 1];
                     array[j + 1] = array[j];
                     array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int[] sortAlter(int[] array) {
+        int n = array.length;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
