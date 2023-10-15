32e7c2a01d19fe6cf30989f69d57d21a9ae75264
EmelKrist <kristina_1516@mail.ru>
2023-10-15 23:25:52 +0800

    feature(easy): Binary Search alter solution

--- LeetcodePractice/src/main/java/easy/BinarySearch.java
+++ LeetcodePractice/src/main/java/easy/BinarySearch.java
@@ -4,13 +4,18 @@ package easy;
  * https://leetcode.com/problems/binary-search/
  */
 public class BinarySearch {
-    public int search(int[] nums, int target) {
-        int lowerBound = 0,  upperBound = nums.length - 1, mid;
-        while (lowerBound <= upperBound) {
-            mid = (lowerBound + upperBound) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) lowerBound = mid + 1;
-            else upperBound = mid - 1;
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int midIndex = (left + right) / 2;
+            int midElem = nums[midIndex];
+            if (midElem == target) {
+                return midIndex;
+            } else if (midElem > target) {
+                right = midIndex - 1;
+            } else {
+                left = midIndex + 1;
+            }
         }
         return -1;
     }
