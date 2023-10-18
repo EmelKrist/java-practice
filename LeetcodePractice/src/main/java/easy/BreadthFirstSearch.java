8e3e31de06ff8027131014175109a5d6bc2477d8
EmelKrist <kristina_1516@mail.ru>
2023-10-18 12:59:12 +0800

    feature(easy): Breadth First Search solution

---
+++ LeetcodePractice/src/main/java/easy/BreadthFirstSearch.java
@@ -0,0 +1,40 @@
+package easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        breadthFirstSearch(root);
+    }
+    public static void breadthFirstSearch(TreeNode root) {
+        if (root == null) return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll(); // извлекаем первый в очереди узел
+            System.out.println(node.val + " ");
+            if (node.left != null) queue.offer(node.left);
+            if (node.right != null) queue.offer(node.right);
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
