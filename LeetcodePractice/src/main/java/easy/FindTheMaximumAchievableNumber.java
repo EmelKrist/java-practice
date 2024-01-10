6fd4923af3e04c7fca512d0abd0855550d7d96d8
EmelKrist <kristina_1516@mail.ru>
2024-01-10 12:49:24 +0800

    feature(easy): Find the maximum achievable number solution

---
+++ LeetcodePractice/src/main/java/easy/FindTheMaximumAchievableNumber.java
@@ -0,0 +1,10 @@
+package easy;
+
+/**
+ * https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+ */
+public class FindTheMaximumAchievableNumber {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
